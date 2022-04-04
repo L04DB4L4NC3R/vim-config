3665a52e02ae6fa5dfabd56442aa5de819ac2e66
= <=>
2022-04-04 23:26:35 +0530

    setup

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git submodule foreach "git pull origin master"
+# Install latest nodejs
+if [ ! -x "$(command -v node)" ]; then
+    curl --fail -LSs https://install-node.now.sh/latest | sh
+    export PATH="/usr/local/bin/:$PATH"
+    # Or use package manager, e.g.
+    # sudo apt-get install nodejs
+fi
+
+# for vim8
+cd ~/.vim/pack/syntax/start
+curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
+# npm install coc-snippets --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+# vim +'CocInstall coc-go coc-pyright' +qall
